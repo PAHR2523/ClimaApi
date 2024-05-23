b6cfab81c8d2cff8baa6d67e33d1f3ba7b570b74
PAHR2523 <hdz63564@gmail.com>
2024-05-23 13:02:58 -0600

    first commit clima

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Predicción del Clima</h3>
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../assets/Spinner.css';
+
+
+const Spinner = () => {
+
+    return(
+        <div className="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div></div>
+    );
+}
+
+export default Spinner;
